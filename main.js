b3e5eb57e65e2d823aa5548a3f971289f70c331c
Plamen <biznetmen@gmail.com>
2022-08-07 23:04:12 +0300

    Moralis initial setup

---
+++ main.js
@@ -0,0 +1,6 @@
+/* Moralis init code */
+const serverUrl = "https://c9lxua1zj3s4.usemoralis.com:2053/server";
+const appId = "fMgEP41CmGg37vAU69Wbfx5ULnJGVNzRmAu9RkVP";
+Moralis.start({ serverUrl, appId });
+
+/* TODO: Add Moralis Authentication code */
