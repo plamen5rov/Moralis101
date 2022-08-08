c31c9d5db7cb5d79d2183e4ce02a199014775905
Plamen <biznetmen@gmail.com>
2022-08-08 11:23:24 +0300

    Moralis transfers dashboard template added in directory 301

--- main.js
+++ main.js
@@ -4,3 +4,27 @@ const appId = "fMgEP41CmGg37vAU69Wbfx5ULnJGVNzRmAu9RkVP";
 Moralis.start({ serverUrl, appId });
 
 /* TODO: Add Moralis Authentication code */
+/* Authentication code */
+async function login() {
+    let user = Moralis.User.current();
+    if (!user) {
+        user = await Moralis.authenticate({
+            signingMessage: "Log in using Moralis",
+        })
+            .then(function (user) {
+                console.log("logged in user:", user);
+                console.log(user.get("ethAddress"));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+
+async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
